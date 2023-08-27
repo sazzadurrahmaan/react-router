41a130bd80a698546cfec0dad96e1b0945f4387b
sazzadurrahman-cse <sazzadrn17@gmail.com>
2023-08-27 11:29:03 +0600

    changed the first page or root page

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,48 +1,50 @@
-import { Outlet, Link } from "react-router-dom";
+import React from 'react';
+import Button from '@mui/material/Button';
+const Root = () => {
+  return (
+    <div>
+       <div id="sidebar" className='pt-5 pl-6 bg-gray-100 p-11 w-96 h-screen'>
+        <div className='flex '>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            />
+            <div
+              id="search-spinner"
+              aria-hidden
+              hidden={true}
+            />
+            <div
+              className="sr-only"
+              aria-live="polite"
+            ></div>
+          </form>
+          <form method="post">
+            <div className='ml-5'>
+              <Button variant="contained"> New</Button>
+            </div>
+          
+          </form>
+        </div>
+        <nav className='mt-10 text-lg font-medium	'>
+          <ul>
+            <li className='my-5'>
+              <a href={`/contacts/1`}>Your Name</a>
+            </li>
+            <li>
+              <a href={`/contacts/2`}>Your Friend</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail"></div>
+    </div>
+  );
+};
 
-export default function Root() {
-    return (
-      <>
-        <div id="sidebar" className="bg-gray-100 w-1/4 p-10 h-auto">
-          <h1>React Router Contacts</h1>
-          <div className="flex ">
-            <form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-                className="shadow appearance-none border rounded-md w-50 py-2 px-3  rounded-md text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-              />
-              <div
-                id="search-spinner"
-                aria-hidden
-                hidden={true}
-              />
-              <div
-                className="sr-only"
-                aria-live="polite"
-              ></div>
-            </form>
-            <form method="post">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-5" type="submit" >New</button>
-            </form>
-          </div>
-          <nav className="mt-10">
-            <ul className="font-2xl font-serif p-5">
-              <li >
-                <Link to={`contact/1`}>Your Name</Link>
-              </li>
-              <li>
-              <Link to={`contact/2`}>Your Friend</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div id="detail">
-        <Outlet />
-        </div>
-      </>
-    );
-  }
+export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import Root from './components/Root/Root.jsx';
-import ErrorPage from './components/Error/Error.jsx';
-import Contact from './components/contact/Contact.jsx';
+import '@fontsource/roboto/400.css';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
-    errorElement: <ErrorPage/>,
-    children: [
-      {
-        path: "contact/",
-        element: <Contact />,
-      },
-    ],
   },
 
 
@@ -28,6 +19,17 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
+/>
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+/>
+
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
